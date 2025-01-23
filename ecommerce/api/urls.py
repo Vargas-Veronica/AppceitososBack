5f69d0ed76f3a694599da57b69db2da48b5872d8
Vargas-Veronica <vargas.veronica55@gmail.com>
2025-01-22 21:24:19 -0300

    Configuro cors

--- ecommerce/api/urls.py
+++ ecommerce/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from api import views
+from api.views import ProductViewSet, CategoryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'ecommerce',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
+    ]
 
 ROOT_URLCONF = 'ecommerce.urls'
 
